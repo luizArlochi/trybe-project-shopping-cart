ee3027d7b02e92bb06c66815773de5244be49f14
Luiz Arlochi <luiz.arlochi@gmail.com>
2023-03-22 18:38:46 -0300

    requisito 3 concluido

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const productsList = async (element) => {
+  const productsSection = document.querySelector('.products');
+  const products = await fetchProductsList(element);
+  products.forEach((product) => {
+    const productElement = createProductElement(product);
+    productsSection.appendChild(productElement);
+  });
+};
+
+window.onload = () => {
+  productsList('computador');
+};
