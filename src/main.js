9803638f4a46600c1c23bf2e5a763366741f5a97
Luiz Arlochi <luiz.arlochi@gmail.com>
2023-03-22 20:20:09 -0300

    requisito 4 concluido

--- src/main.js
+++ src/main.js
@@ -4,15 +4,36 @@ import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+const productsSection = document.querySelector('.products');
+
+const displayLoadingMessage = () => {
+  productsSection.innerHTML = '';
+  const loadingMsg = document.createElement('p');
+  loadingMsg.textContent = 'carregando...';
+  loadingMsg.classList.add('loading');
+  productsSection.appendChild(loadingMsg);
+
+  const removeMessage = () => {
+    loadingMsg.remove();
+  };
+  return removeMessage;
+};
 
 const productsList = async (element) => {
-  const productsSection = document.querySelector('.products');
-  const products = await fetchProductsList(element);
-  products.forEach((product) => {
-    const productElement = createProductElement(product);
-    productsSection.appendChild(productElement);
-  });
+  const removeLoadingMessage = displayLoadingMessage();
+  try {
+    const products = await fetchProductsList(element);
+    products.forEach((product) => {
+      const productElement = createProductElement(product);
+      productsSection.appendChild(productElement);
+    });
+  } catch (error) {
+    console.log(error.message);
+  } finally {
+    removeLoadingMessage();
+  }
 };
+// descobri o finally e estou feliz por ele existir, se o conhecesse antes pouparia uns 30min.
 
 window.onload = () => {
   productsList('computador');
