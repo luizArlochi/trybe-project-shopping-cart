a2197f5d8a21c6da064c29b0e36b1ed62595c88a
Luiz Arlochi <luiz.arlochi@gmail.com>
2023-03-31 16:41:48 -0300

    requisito 11 concluido

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,7 +1,30 @@
-export const getAddress = () => {
-  // seu código aqui
+export const getAddress = async (element) => {
+  const apiRequest = [
+    `https://cep.awesomeapi.com.br/json/${element}`,
+    `https://brasilapi.com.br/api/cep/v2/${element}`,
+  ];
+  let fullAddress = 'CEP não encontrado';
+  try {
+    const response = await Promise.any(apiRequest.map((request) => fetch(request)));
+    const data = await response.json();
+    console.log(data);
+    fullAddress = data.address && data.district && data.city && data.state
+      ? `${data.address} - ${data.district} - ${data.city} - ${data.state}`
+      : `${data.street} - ${data.neighborhood} - ${data.city} - ${data.state}`;
+  } catch (error) {
+    console.log(error.message);
+  }
+  return fullAddress;
 };
 
-export const searchCep = () => {
-  // seu código aqui
+export const searchCep = async () => {
+  const cepInput = document.querySelector('.cep-input');
+  const cartAddress = document.querySelector('.cart__address');
+  const cep = cepInput.value;
+  try {
+    const fullAddress = await getAddress(cep);
+    cartAddress.innerHTML = fullAddress;
+  } catch (error) {
+    console.log(error.message);
+  }
 };
